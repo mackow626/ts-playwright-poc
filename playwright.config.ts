332cfe12482ed410bd03a7062c557cdf287f295c
maciejkowalczuk <maciek.kowalczuk@gmail.com>
2025-10-04 21:46:59 +0200

    drgrossmann test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
-
+import path from 'path';
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -21,12 +21,16 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 
-  reporter: [['allure-playwright'], ['html']],
-
+   outputDir: './test-results', // katalog na screenshoty, trace, nagrania
+   snapshotDir: path.join(__dirname, 'tests/__snapshots__'), // wymuszona ścieżka snapshotów
+   use: {
+     browserName: 'chromium',
+     viewport: { width: 1280, height: 900 },
+     deviceScaleFactor: 1,
+     fullPage: true,
+   },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://demo-bank.vercel.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
@@ -45,46 +49,7 @@ export default defineConfig({
         use: { browserName: 'chromium' },
       },
 
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome']},
-    // },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
